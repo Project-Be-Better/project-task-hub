9904cc1ddad212d96757b606dce935a1d9f5f02a
sree <sreeraj_ec@hotmail.com>
2025-08-29 14:33:44 +0800

    20250829-Note-Service-Unittest-exclude-DB

--- services/note-service/src/test/java/com/keep/notes/NotesServiceApplicationTests.java
+++ services/note-service/src/test/java/com/keep/notes/NotesServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.keep.notes;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(exclude = { DataSourceAutoConfiguration.class })
 class NotesServiceApplicationTests {
 
 	@Test
